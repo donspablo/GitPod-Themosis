fc094e33680ca3296996f368c2822c696c891e2e
don Pablo <don@obeyi.com>
2021-11-19 07:07:06 +0200

    Init

--- scripts/.gitpod-init.sh
+++ scripts/.gitpod-init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo  php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/bin/ --filename=composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
 sudo  ssh-keygen -q -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
 composer install --ignore-platform-reqs
 composer update --ignore-platform-reqs
